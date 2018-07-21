b701cf9d2879f9ff3dbcc189f0023ea80fd987e4
showmyland <elvis08@yeah.net>
2018-07-21 18:33:23 +0800

    增加了部分练习代码

--- test.py
+++ test.py
@@ -1,3 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import socket
 
 print ('hello world')
+
+#name = input('please input your name: ')
+#print("hello,", name)
+
+# num = int(input('please input your number: '))
+# if num < 0:
+#     print(-num)
+# else:
+#     print(num)
+
+# print('''line1
+# line2
+# line3''')
+
+# print(r'''hello,\n
+# world''')
+
+# a = 123 # a是整数
+# print(a)
+# a = 'ABC' # a变为字符串
+# print(a)
+
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print(b)
+
+# print('包含中文')
+
+# print('%2d-%02d' % (3, 1))
+# print('%.2f' % 3.1415926)
+
+# s1 = 72
+# s2 = 85
+# r = '小明的成绩提高了%.1f%%' %((85-72)/72*100)
+# print(r)
+
+
+# L = [
+#     ['Apple', 'Google', 'Microsoft'],
+#     ['Java', 'Python', 'Ruby', 'PHP'],
+#     ['Adam', 'Bart', 'Lisa']
+# ]
+# print(L[0][0])
+# print(L[1][1])
+# print(L[2][2])
+
+# age = 3
+# print('your age is', age)
+# if age >= 18:
+#     print('adult')
+# elif age >= 6:
+#     print('teenager')
+# else:
+#     print('kid')
+
