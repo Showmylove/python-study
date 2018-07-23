152b3952474181dda0bc4ea4704c003d1dc819da
showmyland <elvis08@yeah.net>
2018-07-23 23:52:38 +0800

    调试代码

--- test.py
+++ test.py
@@ -200,31 +200,32 @@ print ('hello world')
 # print(calc(1, 2, 3, 4, 5))
 # print(calc(*[1, 2, 3, 4, 5]))
 
-def product(*args):
-    if len(args) == 0:
-        raise TypeError
-    else:
-        sum = 1
-        for n in args:
-            sum = sum * n
-        return sum
+# def product(*args):
+#     if len(args) == 0:
+#         raise TypeError
+#     else:
+#         sum = 1
+#         for n in args:
+#             sum = sum * n
+#         return sum
 
-# 测试
-print('product(5) =', product(5))
-print('product(5, 6) =', product(5, 6))
-print('product(5, 6, 7) =', product(5, 6, 7))
-print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
-if product(5) != 5:
-    print('测试失败!')
-elif product(5, 6) != 30:
-    print('测试失败!')
-elif product(5, 6, 7) != 210:
-    print('测试失败!')
-elif product(5, 6, 7, 9) != 1890:
-    print('测试失败!')
-else:
-    try:
-        product()
-        print('测试失败!')
-    except TypeError:
-        print('测试成功!')
+# # 测试
+# print('product(5) =', product(5))
+# print('product(5, 6) =', product(5, 6))
+# print('product(5, 6, 7) =', product(5, 6, 7))
+# print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+# if product(5) != 5:
+#     print('测试失败!')
+# elif product(5, 6) != 30:
+#     print('测试失败!')
+# elif product(5, 6, 7) != 210:
+#     print('测试失败!')
+# elif product(5, 6, 7, 9) != 1890:
+#     print('测试失败!')
+# else:
+#     try:
+#         product()
+#         print('测试失败!')
+#     except TypeError:
+#         print('测试成功!')
+
